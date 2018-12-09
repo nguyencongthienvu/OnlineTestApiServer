2bab867143c1d4b15f986c07ded587011b728e43
thienvunguyen <nguyencongthienvu@gmail.com>
2018-12-09 21:41:54 +0700

    Add API for search by mark.

--- controllers/search.js
+++ controllers/search.js
@@ -37,4 +37,30 @@ router.post('/',function(req,res){
     }
  });
 
+ router.post('/searchByMark',function(req,res){
+    if (req.body.min && req.body.max) {
+        deptCourse.query(
+            `SELECT * FROM department dpt, departmentcourse dc, user u, student std, report rpt WHERE u.role = 2 AND dc.dcid = std.dcid AND u.uid = std.uid AND dpt.deptid = std.deptid AND rpt.stdid = std.stdid AND rpt.marks BETWEEN `+req.body.min+` AND `+req.body.max+``
+            ,function(err,rows,fields){
+             if(err){
+                 res.send({errorCode:1,message:"Database Error!",status:"error"});
+             }
+             else{
+                res.send({errorCode:0,message:"successfully",status:"successfully",data:rows});
+             }
+         });
+    } else {
+        deptCourse.query(
+            `SELECT * FROM department dpt, departmentcourse dc, user u, student std, report rpt WHERE u.role = 2 AND dc.dcid = std.dcid AND u.uid = std.uid AND dpt.deptid = std.deptid AND rpt.stdid = std.stdid AND rpt.marks BETWEEN 0 AND 10`
+            ,function(err,rows,fields){
+             if(err){
+                 res.send({errorCode:1,message:"Database Error!",status:"error"});
+             }
+             else{
+                res.send({errorCode:0,message:"successfully",status:"successfully",data:rows});
+             }
+         });
+    }
+ });
+
 module.exports = router
